7c00222cc10d97a6fd25f6880a02241fc9d9e731
Shohil <Sohilupadhyay47@gmail.com>
2021-04-13 14:20:30 +0530

    Nav_graph Completed

---
+++ app/src/main/java/com/example/economist_clone/Fragments/TodayNewsFragments.kt
@@ -0,0 +1,7 @@
+package com.example.economist_clone.Fragments
+
+import androidx.fragment.app.Fragment
+import com.example.economist_clone.R
+
+class TodayNewsFragments : Fragment(R.layout.fragment_today) {
+}
